2315c42033f9b10416538ef8cce5a71752050b32
Zerko3 <zan.zerak99@gmail.com>
2023-04-08 16:37:27 +0200

    Added navigation and JS logic for navigation

--- src/script.js
+++ src/script.js
@@ -1,2 +1,20 @@
 'use strict';
-console.log(1);
+
+// Toggle navigation bar
+
+const hamburger = document.querySelector('.navigation__hamburger');
+console.log(hamburger);
+
+const toggleNavigationBar = function () {
+  console.log('Clicked');
+  document
+    .querySelector('.navigation__hamburger--links')
+    .classList.toggle('hidden');
+};
+
+// INITALIZATION OF THE PAGE
+const init = function () {
+  hamburger.addEventListener('click', toggleNavigationBar);
+};
+
+init();
